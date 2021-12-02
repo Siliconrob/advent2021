26f5e76df864215bd7c7a08f10cc5a12db4b253f
¯\_(ツ)_/¯ <Siliconrob@siliconheaven.net>
2021-12-01 21:57:43 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Advent of Code 2021
+
+- Using `Pycharm` with Python 3.X
+
+I am doing things the weird way so go ahead and criticize
---
+++ day1.py
@@ -0,0 +1,28 @@
+import itertools
+from aocd import get_data
+
+if __name__ == '__main__':
+    #data = [199,200,208,210,200,207,240,269,260,263]
+
+    data = [int(i) for i in list(get_data(day=1).splitlines())]
+
+    pairs = list(itertools.pairwise(data))
+    diffs = [second - first for first, second in pairs]
+    increments = [entry for entry in diffs if entry > 0]
+    print(f'Part 1: Increments: {len(increments)}')
+
+    triples = []
+    # I don't like this, but group does uniques
+    for index, first_value in enumerate(data):
+        if index + 2 >= len(data):
+            break
+        second_value = data[index + 1]
+        third_value = data[index + 2]
+        triples.append((first_value, second_value,third_value))
+
+    triple_sums = [first + second + third for first, second, third in triples]
+    second_pairs = list(itertools.pairwise(triple_sums))
+    diffs = [second - first for first, second in second_pairs]
+    increments = [entry for entry in diffs if entry > 0]
+    print(f'Part 2: Increments: {len(increments)}')
+
