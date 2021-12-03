a1cc96e2dd60988b339307eec7146b864bdb57a6
¯\_(ツ)_/¯ <Siliconrob@siliconheaven.net>
2021-12-02 19:05:05 -0600

    Now with part 2

--- day2.py
+++ day2.py
@@ -6,6 +6,7 @@ from parse import parse
 class Position:
     horizontal: int = 0
     vertical: int = 0
+    aim: int = 0
 
     def distance(self) -> int:
         return self.horizontal * self.vertical
@@ -32,14 +33,21 @@ if __name__ == '__main__':
         direction, value = parse("{} {}", line)
         movements.append(Movement(direction, int(value)))
 
-    dive_tracker1 = Position()
+    dive_tracker = Position()
+    aimed_tracker = Position()
+
     for movement in movements:
         to_add = movement.unit
         if movement.type == "up":
             to_add = to_add * -1
         if movement.type in ["up", "down"]:
-            dive_tracker1.vertical = dive_tracker1.vertical + to_add
+            dive_tracker.vertical = dive_tracker.vertical + to_add
+            aimed_tracker.aim = aimed_tracker.aim + to_add
         else:
-            dive_tracker1.horizontal = dive_tracker1.horizontal + to_add
-    print(f"Part 1 {dive_tracker1.distance()}")
+            dive_tracker.horizontal = dive_tracker.horizontal + to_add
+            aimed_tracker.horizontal = aimed_tracker.horizontal + to_add
+            aimed_tracker.vertical = aimed_tracker.aim * to_add + aimed_tracker.vertical
+
+    print(f"Part 1 {dive_tracker.distance()}")
+    print(f"Part 2 {aimed_tracker.distance()}")
 
