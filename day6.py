ddd95ef7db9afd08eb10592c988f705db748ce12
¯\_(ツ)_/¯ <Siliconrob@siliconheaven.net>
2021-12-06 17:48:19 -0600

    Part 2 Day 6

--- day6.py
+++ day6.py
@@ -7,6 +7,7 @@ if __name__ == '__main__':
     fish = np.array([int(x) for x in data.split(',')])
     current_fish_count = fish.size
 
+    # the hard way grows the list exponentially
     for day in range(80):
         move_day = lambda z: z - 1
         fish = move_day(fish)
@@ -18,3 +19,19 @@ if __name__ == '__main__':
             fish = np.append(fish, to_add)
         fish = np.where(fish < 0, 6, fish)
     print(f'Part 1: {fish.size}')
+
+    # only care about the day totals so move that along
+    fish = [int(x) for x in data.split(',')]
+    spawn_days = []
+    for spawn_day in range(9):
+        count = sum(map(lambda x: x == spawn_day, fish))
+        spawn_days.append(count)
+
+    for day in range(256):
+        to_add = spawn_days[0]
+        spawn_days = spawn_days[1:]
+        spawn_days.append(to_add)
+        spawn_days[6] += to_add
+    print(f'Part 2: {sum(spawn_days)}')
+
+
