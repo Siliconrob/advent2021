a28d37d1cea601773efeee8238faa450a5f1cbbe
¯\_(ツ)_/¯ <Siliconrob@siliconheaven.net>
2021-12-06 23:51:20 -0600

    Setup for part 2

--- day7.py
+++ day7.py
@@ -4,15 +4,17 @@ from aocd import get_data
 if __name__ == '__main__':
     #data = "16,1,2,0,4,2,7,1,2,14"
     data = get_data(day=7, year=2021)
-
     data_points = np.array([int(x) for x in data.split(',')])
     data_points.sort()
-    current_distance = np.inf
+
+    current_distance_part1 = np.inf
+    current_distance_part2 = np.inf
 
     for current_target in range(data_points.min(), data_points.max()):
-        move_fn = lambda z: abs(z - current_target)
-        fuel_costs = move_fn(data_points)
-        fuel_cost = np.sum(fuel_costs).squeeze()
-        if current_distance > fuel_cost:
-            current_distance = fuel_cost
-    print(current_distance)
+        move_fn_part1 = lambda z: abs(z - current_target)
+        fuel_costs_part1 = move_fn_part1(data_points)
+        fuel_cost_part1 = np.sum(fuel_costs_part1).squeeze()
+        if current_distance_part1 > fuel_cost_part1:
+            current_distance_part1 = fuel_cost_part1
+    print(f'Part 1 {current_distance_part1}')
+    print(f'Part 2 {current_distance_part2}')
