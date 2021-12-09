a5753e03bf22c8f060d85f5a28e404d29edf2333
¯\_(ツ)_/¯ <Siliconrob@siliconheaven.net>
2021-12-08 21:31:19 -0600

    Day 8 Part 2

--- day8.py
+++ day8.py
@@ -5,17 +5,27 @@ from aocd import get_data
 
 @dataclass
 class InputLine:
-    line: str
+    line: str = ""
 
     def signals(self) -> list:
-        return str.strip(self.line.split('|').pop(0)).split(' ')
+        return ["".join(sorted(result)) for result in str.strip(self.line.split('|').pop(0)).split(' ')]
 
     def output(self) -> list:
-        return str.strip(self.line.split('|').pop()).split(' ')
+        return ["".join(sorted(result)) for result in str.strip(self.line.split('|').pop()).split(' ')]
+
+@dataclass
+class InputSignal:
+    A: str = ''
+    B: str = ''
+    C: str = ''
+    D: str = ''
+    E: str = ''
+    F: str = ''
+    G: str = ''
 
 @dataclass
 class DigitDisplay:
-    Zero:str = "abcefg"
+    Zero: str = "abcefg"
     One: str = "cf"
     Two: str = "acdeg"
     Three: str = "acdfg"
@@ -26,6 +36,10 @@ class DigitDisplay:
     Eight: str = "abcdefg"
     Nine: str = "abcdfg"
 
+    def match(self, output_value: str) -> int:
+        options = [self.Zero, self.One, self.Two, self.Three, self.Four, self.Five, self.Six, self.Seven, self.Eight, self.Nine]
+        return options.index(output_value)
+
 
 def part1_count(output_values: list, search_digit_patterns: list) -> int:
     total_uniques = 0
@@ -34,6 +48,58 @@ def part1_count(output_values: list, search_digit_patterns: list) -> int:
         output_digits = [len(output_digit) for output_digit in output_value]
         total_uniques += sum([v if k in digit_lengths else 0 for k, v in Counter(output_digits).items()])
     return total_uniques
+
+
+def create_signal_input(signal_line: list) -> InputSignal:
+    # Known pattern signals
+    signal_cf = set(list(filter(lambda x: (len(x) == 2), signal_line)).pop())  # 1
+    signal_bcdf = set(list(filter(lambda x: (len(x) == 4), signal_line)).pop())  # 4
+    signal_acf = set(list(filter(lambda x: (len(x) == 3), signal_line)).pop())  # 7
+    signal_abcdefg = set(list(filter(lambda x: (len(x) == 7), signal_line)).pop())  # 8
+
+    # multiple options
+    three_five_two_options = five_segment_options = list(filter(lambda x: (len(x) == 5), signal_line))
+    zero_six_nine_options = list(filter(lambda x: (len(x) == 6), signal_line))
+
+    # five signal pattern common elements
+    signal_adg = set.intersection(*map(set, three_five_two_options))
+    # six signal pattern common elements
+    signal_bdefg = set.intersection(*map(set, zero_six_nine_options))
+
+    # make a lookup table
+    signal_table = InputSignal()
+    signal_table.A = set.difference(signal_acf, signal_cf)
+    signal_table.B = set.difference(set.difference(signal_bcdf, signal_cf), signal_adg)
+    signal_table.C = set.difference(signal_cf, signal_bdefg)
+    signal_table.D = set.intersection(signal_bcdf, signal_adg)
+    signal_table.E = set.difference(set.difference(signal_abcdefg, signal_adg), signal_bcdf)
+    signal_table.F = set.intersection(signal_bdefg, signal_cf)
+    signal_table.G = set.difference(set.intersection(signal_bdefg, signal_adg), signal_bcdf)
+    return signal_table
+
+
+def new_display_lines(signal_table: InputSignal) -> DigitDisplay:
+    # reassign the input signal characters according to set intersection, union, difference of common signals
+    current_display = DigitDisplay()
+    current_display.Zero = "".join(sorted(
+        set.union(signal_table.A, signal_table.B, signal_table.C, signal_table.E, signal_table.F, signal_table.G)))
+    current_display.One = "".join(sorted(set.union(signal_table.C, signal_table.F)))
+    current_display.Two = "".join(
+        sorted(set.union(signal_table.A, signal_table.C, signal_table.D, signal_table.E, signal_table.G)))
+    current_display.Three = "".join(
+        sorted(set.union(signal_table.A, signal_table.C, signal_table.D, signal_table.F, signal_table.G)))
+    current_display.Four = "".join(sorted(set.union(signal_table.B, signal_table.C, signal_table.D, signal_table.F)))
+    current_display.Five = "".join(
+        sorted(set.union(signal_table.A, signal_table.B, signal_table.D, signal_table.F, signal_table.G)))
+    current_display.Six = "".join(sorted(
+        set.union(signal_table.A, signal_table.B, signal_table.D, signal_table.E, signal_table.F, signal_table.G)))
+    current_display.Seven = "".join(sorted(set.union(signal_table.A, signal_table.C, signal_table.F)))
+    current_display.Eight = "".join(sorted(
+        set.union(signal_table.A, signal_table.B, signal_table.C, signal_table.D, signal_table.E, signal_table.F,
+                  signal_table.G)))
+    current_display.Nine = "".join(sorted(
+        set.union(signal_table.A, signal_table.B, signal_table.C, signal_table.D, signal_table.F, signal_table.G)))
+    return current_display
 
 
 if __name__ == '__main__':
@@ -52,6 +118,17 @@ if __name__ == '__main__':
     data = get_data(day=8, year=2021).splitlines()
 
     digits = DigitDisplay()
-    data_lines = [InputLine(line) for line in data]
-    part1_result = part1_count([data_line.output() for data_line in data_lines], [digits.One, digits.Four, digits.Seven, digits.Eight])
+    inputs = [InputLine(line) for line in data]
+    outputs = [input.output() for input in inputs]
+    part1_result = part1_count(outputs, [digits.One, digits.Four, digits.Seven, digits.Eight])
     print(f'Part 1: {part1_result}')
+
+    part2_result = 0
+    for input in inputs:
+        outputs = input.output()
+        # lookup the values for this iteration display
+        current_display = new_display_lines(create_signal_input(input.signals()))
+        line_entry_value = int("".join([str(current_display.match(output_value)) for output_value in outputs]))
+        print(line_entry_value)
+        part2_result += line_entry_value
+    print(f'Part 2: {part2_result}')
