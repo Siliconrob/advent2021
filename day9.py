0d529851adb3a6d70764ab51711870ba59d56794
¯\_(ツ)_/¯ <Siliconrob@siliconheaven.net>
2021-12-09 20:48:38 -0600

    Day 9 Part 2

--- day9.py
+++ day9.py
@@ -1,7 +1,58 @@
-from collections import Counter
+from collections import deque
 from dataclasses import dataclass
+import numpy as np
+import numpy
 import pandas
 from aocd import get_data
+
+@dataclass
+class Point:
+    row: int = np.inf
+    column: int = np.inf
+
+
+@dataclass
+class LowPoints(Point):
+    value: int = np.inf
+
+@dataclass
+class Bounds:
+    min_row: int = np.inf
+    max_row: int = np.inf
+    min_column: int = np.inf
+    max_column: int = np.inf
+
+
+def possible_neighbors(row: int, column: int) -> list:
+    return [
+        (column - 1, row),
+        (column + 1, row),
+        (column, row - 1),
+        (column, row + 1)
+    ]
+
+
+def find_low_points(the_grid: pandas.DataFrame) -> (list, Bounds):
+    low_points = []
+    the_grid_bounds = Bounds()
+    the_grid_bounds.min_column = min(the_grid.columns)
+    the_grid_bounds.max_column = max(the_grid.columns)
+    for col_index in range(the_grid.columns.size):
+        for row_index in range(the_grid[col_index].size):
+            target_cell = the_grid.iat[row_index, col_index]
+            adjacency = []
+            if the_grid_bounds.min_row == np.inf:
+                the_grid_bounds.min_row = 0
+                the_grid_bounds.max_row = the_grid[col_index].count() - 1
+            for col_x, row_y in possible_neighbors(row_index, col_index):
+                if col_x < the_grid_bounds.min_column or col_x > the_grid_bounds.max_column:
+                    continue
+                if row_y < the_grid_bounds.min_row or row_y > the_grid_bounds.max_row:
+                    continue
+                adjacency.append(the_grid.iat[row_y, col_x])
+            if all(x > target_cell for x in adjacency):
+                low_points.append(LowPoints(row=row_index, column=col_index, value=target_cell))
+    return low_points, the_grid_bounds
 
 
 if __name__ == '__main__':
@@ -16,34 +67,26 @@ if __name__ == '__main__':
 
     data_points = [[int(point) for point in line] for line in data]
     grid = pandas.DataFrame(data_points)
-    low_points = []
-    min_col_x = min(grid.columns)
-    max_col_x = max(grid.columns)
-    for col_index in range(grid.columns.size):
-        for row_index in range(grid[col_index].size):
-            target_cell = grid.iat[row_index, col_index]
-            adjacency = []
-            min_row_y = 0
-            max_row_y = grid[col_index].count() - 1
+    low_points, bounds = find_low_points(grid)
+    part1_answer = sum([low_point.value + 1 for low_point in low_points])
+    print(f'Part 1 {part1_answer}')
 
-            possibilities = [
-                (col_index - 1, row_index),
-                 (col_index + 1, row_index),
-                 (col_index, row_index - 1),
-                 (col_index, row_index + 1)
-            ]
-            for col_x, row_y in possibilities:
-                if col_x < min_col_x or col_x > max_col_x:
+    basins = []
+    for low_point in low_points:
+        start_location = Point(low_point.row, low_point.column)
+        # print(start_location)
+        cells_to_check = deque([start_location])
+        temp_grid = grid.copy(deep=True)
+        while len(cells_to_check) > 0:
+            target_cell = cells_to_check.popleft()
+            temp_grid.iat[target_cell.row, target_cell.column] = np.inf
+            for col_x, row_y in possible_neighbors(target_cell.row, target_cell.column):
+                if col_x < bounds.min_column or col_x > bounds.max_column:
                     continue
-                if row_y < min_row_y or row_y > max_row_y:
+                if row_y < bounds.min_row or row_y > bounds.max_row:
                     continue
-                try:
-                    adjacency.append(grid.iat[row_y, col_x])
-                except:
-                    print(col_x)
-                    print(row_y)
-                    print(target_cell)
-            if all(x > target_cell for x in adjacency):
-                low_points.append(target_cell)
-    risk = sum([low_point + 1 for low_point in low_points])
-    print(f'Part 1 {risk}')
+                if temp_grid.iat[row_y, col_x] < 9:
+                    cells_to_check.append(Point(row_y, col_x))
+        basins.append(temp_grid.eq(np.inf).sum().sum())
+        basins.sort()
+    print(f'Part 2: {numpy.prod(basins[-3:])}' )
