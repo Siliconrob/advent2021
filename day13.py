d90ce1032197808605a9b4045b035d4173a77316
¯\_(ツ)_/¯ <Siliconrob@siliconheaven.net>
2021-12-13 00:39:48 -0600

    Part 2 output in commit
    ###  #### #  # ###  #  # ###  #  # ###
    #  # #    #  # #  # #  # #  # # #  #  #
    #  # ###  #  # #  # #  # #  # ##   #  #
    ###  #    #  # ###  #  # ###  # #  ###
    # #  #    #  # #    #  # #    # #  # #
    #  # ####  ##  #     ##  #    #  # #  #

--- day13.py
+++ day13.py
@@ -1,6 +1,7 @@
 from aocd import get_data
 
-
+# Wrote this printout function to do compare with Part 1 test outputs
+# luckily that was the part 2.  WoooHOOO
 def printout(current_paper: dict, fold_cmd: str, max_x: int, max_y: int):
     in_process = [["." for x in range(max_x)] for y in range(max_y)]
     for dot in current_paper:
@@ -42,7 +43,7 @@ if __name__ == '__main__':
     #folds = [(fold[0][-1], int(fold[-1])) for fold in [fold_instr.split("=") for fold_instr in test_data_folds]]
     #dots = [(int(pos[0]), int(pos[1])) for pos in [coord.split(",") for coord in test_data_dots]]
 
-    paper = {}
+    paper = {} # use dictionary because coords are jagged
     for dot in dots:
         paper[(dot[0], dot[1])] = True
 
@@ -51,16 +52,17 @@ if __name__ == '__main__':
     max_y = max([key[1] for key in the_keys]) + 1
 
     for index, fold in enumerate(folds):
-        if index == 1:
+        if index == 1: # Part 1 answer only
             printout(paper, fold, max_x, max_y)
             part1_solution = len(paper.keys())
         fold_axis, fold_value = fold
         folded_paper = {}
         if fold_axis == "x":
             for (x, y) in paper:
-                if x < fold_value:
+                if x < fold_value: # coord is above the fold
                     folded_paper[(x, y)] = True
                 else:
+                    # Subtract the fold value from the current coord to mirror it
                     folded_x = fold_value - (x - fold_value)
                     print(f'Change from {x} to {folded_x}')
                     folded_paper[(folded_x, y)] = True
@@ -70,6 +72,7 @@ if __name__ == '__main__':
                 if y < fold_value:
                     folded_paper[(x, y)] = True
                 else:
+                    # Subtract the fold value from the current coord to mirror it
                     folded_y = fold_value - (y - fold_value)
                     print(f'Change from {y} to {folded_y}')
                     folded_paper[(x, folded_y)] = True
